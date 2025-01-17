0cad7fe27588e00d2f226140685e47c9018c1022
Elijah Crum <elijah@crums.us>
2025-01-16 21:13:07 -0600

    Add code from codepen

--- main.js
+++ main.js
@@ -1,1 +1,91 @@
-function main()
+function pause(s = 1) {
+    return new Promise(resolve => setTimeout(resolve, 1000 * Number(s)));
+}
+
+async function type(text) {
+
+    let queue = text.split("");
+    let container = document.getElementById("terminal")
+
+    while (queue.length) {
+        let char = queue.shift();
+        container.appendChild(char);
+        await pause(0.01);
+    }
+
+    await pause(0.1);
+    container.classList.remove("active");
+    return;
+}
+
+
+async function parse(input) {
+
+    // Only allow words, separated by space
+	let matches = String(input).match(/^(\w+(?:(?:\s|-)\w+)*)$/);
+
+	if (!matches) {
+		throw new Error("Invalid command");
+	}
+	let command = matches[1];
+	let args = matches[2];
+
+    let module;
+
+    // Try to import the command function
+    try {
+        module = await import(`./commands/${command}.js`);
+    } catch (e) {
+        console.error(e);
+
+        if (e instanceof TypeError) {
+            e.message = `Unknown command: ${command}`;
+            type("Command not found")
+        }
+		else {
+			e.message = "Error while executing command";
+		}
+		throw e;
+    }
+
+    // Show any output if the command exports any
+	await type(module.output);
+	await pause();
+
+	// Execute the command (default export)
+	await module.default?.(args);
+
+	return;
+}
+
+async function input(pw) {
+    return new Promise(resolve => {
+        const onKeyDown = event => {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                let result = event.target.textContent;
+                resolve(result);
+            }
+        };
+
+        let terminal = document.querySelector(".terminal");
+        let input = document.createElement("div");
+        input.setAttribute("id", "input");
+        input.setAttribute("contenteditable", true);
+        input.addEventListener("keydown", onKeyDown);
+        terminal.appendChild(input);
+        input.focus();
+    });
+}
+
+async function main() {
+    let command = await input()
+    await parse(command);
+
+    main();
+}
+
+window.onload = function() {
+    main();
+  };
+  
